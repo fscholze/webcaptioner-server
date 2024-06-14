ac7b120481cab577319a7f25479330102d70dba4
Daniel Zoba <daniel.sobe@sorben.com>
2024-06-14 12:54:40 +0000

    proper start cmd

--- docker/startme.sh
+++ docker/startme.sh
@@ -5,4 +5,4 @@ echo "PORT=$WEBCAPTIONER_BACKEND_PORT"  > .env
 echo "SOTRA_SERVER_URL=$SOTRA_SERVER_URL" >> .env
 popd
 
-cd webcaptioner-ng-back && npm start
+cd webcaptioner-ng-back && npm run dev
