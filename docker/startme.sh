5ea6daf391033abc60c777657b95b4fb78392f7d
Daniel Zoba <daniel.sobe@sorben.com>
2024-06-14 14:40:13 +0200

    simple dockerization

---
+++ docker/startme.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pushd webcaptioner-ng-back
+echo "PORT=$WEBCAPTIONER_BACKEND_PORT"  > .env
+echo "SOTRA_SERVER_URL=$SOTRA_SERVER_URL" >> .env
+popd
+
+cd webcaptioner-ng-back && npm start
