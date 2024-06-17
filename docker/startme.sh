510feda0f2f742390cb7743341211cd35e68516c
Daniel Zoba <daniel.sobe@sorben.com>
2024-06-17 14:57:07 +0000

    clean up config

--- docker/startme.sh
+++ docker/startme.sh
@@ -3,6 +3,7 @@
 pushd webcaptioner-ng-back
 echo "PORT=$WEBCAPTIONER_BACKEND_PORT"  > .env
 echo "SOTRA_SERVER_URL=$SOTRA_SERVER_URL" >> .env
+echo "VOSK_SERVER_URL=$VOSK_SERVER_URL" >> .env
 popd
 
 cd webcaptioner-ng-back && npm run dev
